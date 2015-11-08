2c81543a90df82f4651d324e5ea4daab1cfa54ea
cgonzalezg <cesargonz1984@gmail.com>
2015-11-08 02:36:56 +0100

    add relation

--- logic/capture.js
+++ logic/capture.js
@@ -3,18 +3,31 @@ var async = require('async');
 var Parse = require('parse/node').Parse;
 var Tag = Parse.Object.extend("Tag");
 var Player = Parse.Object.extend("Player");
+var Team = Parse.Object.extend("Team");
+var User = Parse.Object.extend("User");
 
 Parse.initialize("VeEodLh11HU4otvwIHpl3slzqN21jYFRefHMQPvp", "ja0EMrZH0mdE8TBRFrIXPCA54SYCF1fCgn04uXgI");
 
+function saveTagPerUser(tags) {
+  async.each(tags, function(tag, cb) {
+    var query = new Parse.Query(Player);
+    var relation = query.relation("user").query();
+    relation.first({
+      success: function (user) {
+        user.set('totalTags', tags.length);
+      }
+    });
+  });
+}
 
 function checkWinner(gameId, callback) {
-  var query = new Parse.Quary(Tag);
+  var query = new Parse.Query(Tag);
   query.find({
     success: function(tags) {
-      var teamsScore = _.groupBy(tags, 'teamId');
+      var teamsScore = _.groupBy(tags, 'team');
       for (var key in teamsScore) {
         if (teamsScore[key].length > tags.length / 2) {
-
+          saveTagPerUser(tags);
           return callback(null, teamsScore[key]);
         }
       }
@@ -36,8 +49,8 @@ function calculateDomination(tagId, teamId, userId, callback) {
     console.log(result);
     if (!result) return callback(null, null);
     if (!result.userId) {
-      result.set("userId", userId);
-      result.set("teamId", teamId);
+      result.set("user", userId);
+      result.set("team", teamId);
       result.save(function(err, data) {
         return callback(err, data);
       });
@@ -51,7 +64,7 @@ function calculateDomination(tagId, teamId, userId, callback) {
 
 
     queryNotTeam.withinKilometers("location", tagLocation, 0.05);
-    queryNotTeam.notEqualTo("teamId", teamId);
+    queryNotTeam.notEqualTo("team", teamId);
 
     queryNotTeam.count().then(
       function(count) {
@@ -68,12 +81,16 @@ function calculateDomination(tagId, teamId, userId, callback) {
         queryTeamMembers.count({
           success: function(countTeam) {
             if (count === countTeam) {
-              result.set("userId", null);
-              result.set("teamId", null);
+              result.set("player", null);
+              result.set("team", null);
             }
             if (count < countTeam) {
-              result.set("userId", userId);
-              result.set("teamId", teamId);
+              var queryTeam = new Parse.Query(Team);
+              queryTeam.get(teamId, function(err, data) {
+                result.set("player", userId);
+                result.set("team", data);
+                return result.save(callback);
+              });
             }
             return result.save(callback);
           }
@@ -84,7 +101,7 @@ function calculateDomination(tagId, teamId, userId, callback) {
 
 function capture(req, resp) {
   console.log(req.body.tagId, req.body.teamId, req.body.userId);
-  calculateDomination(req.body.tagId, req.body.teamId, req.body.userId, function(err, data) {
+  calculateDomination(req.body.tagId, req.body.teamId, req.body.playerId, function(err, data) {
     console.log(data);
     if (err || !data)
       return resp.status(403).end();
